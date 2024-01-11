c38b534ff6471c9f24c7f158ad33121faed22498
pradeep0148 <122095172+pradeep0148@users.noreply.github.com>
2024-01-11 12:40:05 -0500

    Add files via upload

---
+++ querySelector & querySelectorAll Methods/index.js
@@ -0,0 +1,27 @@
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "gray";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.listStyleType = "none";
+
+const basketHeading = document.querySelector("h2");
+basketHeading.style.marginLeft = "30px";
+
+const fruitItems = document.querySelectorAll(".fruit");
+for (let i = 0; i < fruitItems.length; i++) {
+  fruitItems[i].style.backgroundColor = "white";
+  fruitItems[i].style.padding = "10px";
+  fruitItems[i].style.margin = "10px";
+  fruitItems[i].style.borderRadius = "5px";
+}
+
+document.querySelector("#basket-heading").style.color = "brown";
+document.querySelectorAll(".fruit:nth-child(even)").forEach((fruit) => {
+  fruit.style.backgroundColor = "brown";
+  fruit.style.color = "white";
+});
